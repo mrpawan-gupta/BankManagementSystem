529974e48425f0be00be4a1ee60c943b65301961
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-10 20:01:36 +0530

    Connection to Databse Created

--- src/bankmanagementsystem/DatabaseConnection.java
+++ src/bankmanagementsystem/DatabaseConnection.java
@@ -2,28 +2,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package bankmangementsystem;
+package bankmanagementsystem;
 
 import java.sql.*;
-/**
+/*
  *
- * @author pawan
+ * @author notyourpawan
  */
 public class DatabaseConnection {
-
-    Connection conn;
-    Statement statement;
-
+    static final String DB_URL = "jdbc:mysql://localhost/bankmanagementsystem";
+    static final String USER = "root";
+    static final String PASS = "root";
+    Connection connect;
+    Statement stmt;
     public DatabaseConnection(){
-      try {
-        conn = DriverManager.getConnection("jdbc:mysql:///DB_Bank","root","root");
-        statement = conn.createStatement();
-    } catch (Exception e) {
-        //TODO: handle exception
-        System.out.println(e);
-      }
+        try{
+            //Register Driver
+            //Class.forName(com.mysql.cj.jdbc.Driver);
+            //Create connection
+            connect = DriverManager.getConnection(DB_URL, USER, PASS);
+            //Create statement
+            stmt = connect.createStatement();
+        }catch(Exception exception){
+            System.out.println(exception);
+        }
     }
-    public static void main(String[] args) {
-      new DatabaseConnection();
+    public static void main(String[] args){
+        DatabaseConnection c = new DatabaseConnection();
     }
+    
 }
