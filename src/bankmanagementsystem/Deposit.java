83d03a22a18c6493ba2f61784bcc0ddd0eaec2b3
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-13 22:18:27 +0530

    Deposit Feature UI Updated

--- src/bankmanagementsystem/Deposit.java
+++ src/bankmanagementsystem/Deposit.java
@@ -22,39 +22,39 @@ public class Deposit extends JFrame implements ActionListener{
 
     public Deposit(String pinnumber){
       this.pinnumber = pinnumber;
-
-      ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
-        //Scale the image
-      Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
-        //Convert it into image icon
-      ImageIcon i3 = new ImageIcon(i2);
-      JLabel image = new JLabel(i3);
-      image.setBounds(0, 0, 900, 900);
-      add(image);
-
-      JLabel text = new JLabel("Enter the amount you want to deposit");
-      text.setForeground(Color.WHITE);
-      text.setFont(new Font("System", Font.BOLD, 16));
-      text.setBounds(170,300,400,200);
-
-      amount = new JTextField();
-      amount.setFont(new Font("System", Font.BOLD, 20));
-      amount.setBounds(170, 350,320,25);
-      image.add(amount);
-
-      deposit = new JButton("Deposit");
-      deposit.setBounds(355,485,150,30);
-      deposit.addActionListener(this);
-      image.add(deposit);
-
-      back = new JButton("Back");
-      back.setBounds(355,520,150,30);
-      back.addActionListener(this);
-      image.add(back);
-
-      setSize(900,900);
-      setLocation(300,0);
-      setVisible(true);
+      setLayout(null);
+        //Add Image
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
+        
+        JLabel text = new JLabel("Enter the amount you want to deposit");
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("Abyssinica SIL", Font.BOLD, 16));
+        text.setBounds(170, 300, 400, 20);
+        image.add(text);
+        
+        amount = new JTextField();
+        amount.setFont(new Font("Abyssinica SIL", Font.BOLD, 22));
+        amount.setBounds(170, 350, 320, 25);
+        image.add(amount);
+        
+        deposit = new JButton("Deposit");
+        deposit.setBounds(355, 485, 150, 30);
+        deposit.addActionListener(this);
+        image.add(deposit);
+        
+        back = new JButton("Back");
+        back.setBounds(355, 520, 150, 30);
+        back.addActionListener(this);
+        image.add(back);
+        
+        setSize(900, 900);
+        setLocation(300, 0);
+        setVisible(true);
     }
     public void actionPerformed(ActionEvent ae) {
       // TODO Auto-generated method stub
