41d627976a162c5091be69a72288798e5106191f
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-14 19:39:42 +0530

    Initial Balance ENquiry Class

---
+++ src/bankmanagementsystem/BalanceEnquiry.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankmanagementsystem;
+
+/**
+ *
+ * @author notyourpawan
+ */
+public class BalanceEnquiry {
+    public BalanceEnquiry(String string) {
+    
+    
+    }
+
+    public static void main(String[] args) {
+      new BalanceEnquiry("");
+    }
+}
